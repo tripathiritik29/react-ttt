211ab34006ad5a5379f17dcb09b669907d79615a
tripathiritik29 <ritiktripathi29@gmail.com>
2020-06-06 15:06:52 +0530

    Updates

---
+++ precache-manifest.2e85c30f5b96aec47f0ad82aa12fcd21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3e452795a680de3bb3889317c52e514",
+    "url": "/react-ttt/index.html"
+  },
+  {
+    "revision": "b2f96cbd7295e4ae4d9d",
+    "url": "/react-ttt/static/css/main.2b16f81d.chunk.css"
+  },
+  {
+    "revision": "f69d6f06934987a95927",
+    "url": "/react-ttt/static/js/2.cc2f6224.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-ttt/static/js/2.cc2f6224.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2f96cbd7295e4ae4d9d",
+    "url": "/react-ttt/static/js/main.b6f55b8f.chunk.js"
+  },
+  {
+    "revision": "196bcc74dbca9a514f0f",
+    "url": "/react-ttt/static/js/runtime-main.d63b06da.js"
+  }
+]);
